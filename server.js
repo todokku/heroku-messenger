a4b78b15692672e73e293966ae3a7be039199451
anant sharma <anant.sharma592@gmail.com>
2020-04-21 01:24:29 +0530

    set postback

--- server.js
+++ server.js
@@ -135,11 +135,10 @@ const callSendAPI = async (sender_psid, content) => {
       },
       "message": content
     }
-    const response =await fetch('https://graph.facebook.com/v6.0/me/messages', {
+    const response =await fetch('https://graph.facebook.com/v6.0/me/messages?access_token=EAADhAkZCgj7QBAFwncIYuPjvAixZBQXvTKVXMQwWrqeJZA5vF21OYXWn3Cg438fXygJLZAWMaeHRUvDUTBmU3BgmPzniNKkJPKoZB2VFT5g0tllDvDppqUFsOvMsEsCmqsf3rnDZBxJrInuPqnZAJMzSV48tEUsEcZCZCeyZCwhgf7vtVn2C2ib1fKYE1EwPpiJr0ZD', {
         method: 'post',
         body:    JSON.stringify(request_body),
         headers: {"Content-Type": "application/json"},
-        params : {"access_token" : "EAADhAkZCgj7QBAFwncIYuPjvAixZBQXvTKVXMQwWrqeJZA5vF21OYXWn3Cg438fXygJLZAWMaeHRUvDUTBmU3BgmPzniNKkJPKoZB2VFT5g0tllDvDppqUFsOvMsEsCmqsf3rnDZBxJrInuPqnZAJMzSV48tEUsEcZCZCeyZCwhgf7vtVn2C2ib1fKYE1EwPpiJr0ZD"}
     })
    // .then(res => res.json())
    // .then(json => console.log(json));
